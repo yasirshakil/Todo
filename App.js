861a05224f1878dc881b8acff4ad6f10fc16294b
yasirshakil <m.yasirshakil@gmail.com>
2019-03-30 01:01:47 -0700

    I want to check

--- App.js
+++ App.js
@@ -1,19 +1,64 @@
 import React, { Component } from 'react';
-import { AppRegistry, Image, View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { AppRegistry, Image, View, Text, StyleSheet, TextInput, TouchableOpacity, ScrollView , Button} from 'react-native';
 
 import Login from './app/pages/Login';
+import Banana from './app/pages/banana';
+import FixedDimensionsBasic from './app/pages/FixedDimensionsBasic';
+import FlexDirectionBasics from './app/pages/FlexDirectionBasics';
+import PizzaTranslator from './app/pages/PizzaTranslator';
+import FlatListBasics from './app/pages/FlatList Basics';
+import FetchExample from './app/pages/FetchExample';
 
 export default class Bananas extends Component {
   render() {
     
     return (
-<View style={styles.container}>
-    <Image
-      style={{alignItems: "center", justifyContent:"center", width: 50, height: 50}}
-      source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}} />
-      <Text style={styles.logotext}> TODO </Text>
+         
+      <ScrollView>
      <Login />
-     </View>
+
+     <View style={styles.container}> 
+       <Text style={styles.Text} >this is Image</Text>
+    <Banana />
+    </View>
+
+    <View style={styles.container}>
+      <Text style={styles.Text}> This is Fixed FixedDimensions </Text>
+    <FixedDimensionsBasic /> 
+    </View>
+
+    <View style={styles.container}>
+      <Text style={styles.Text}> This is PizzaTranslator </Text>
+    <PizzaTranslator />
+    </View>
+
+
+    <View style={styles.container}>
+      <Text style={styles.Text}> This is FixedDimensionsBasic </Text>
+    <FlexDirectionBasics />
+      </View>
+
+      <View style={styles.container}>
+      <Text style={styles.Text}> This is Button  </Text>
+
+    <Button
+  onPress={() => {
+    Alert.alert('You tapped the button!');
+  }}
+  title="Press Me"
+/>
+</View>
+
+<View style={styles.container}>
+<Text style={styles.Text}> This is FlatListBasics  </Text>
+<FlatListBasics />
+</View>
+
+<FetchExample />
+
+     </ScrollView>
+     
+
     );
   }
 }
@@ -30,5 +75,9 @@ const styles = StyleSheet.create({
     fontSize: 18,
     marginVertical: 15,
   },
+  Text: {
+    fontSize: 18,
+    color: 'green',
+  }
  
 });
